01cd967d0e3c45e314845ace7248858a2e7d40c1
Rob Mills <rob.mills@uptake.com>
2016-06-20 23:30:01 -0500

    Fix issues with miswired repository

--- src/main/java/com/rgm/Application.java
+++ src/main/java/com/rgm/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Entry point for the Spring Boot application.
@@ -17,6 +18,7 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 @ComponentScan
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.rgm.user")
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
